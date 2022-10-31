88f0267ae9650fe0daf1643e014d6c8f1857238f
humbertovacu <humbertovacu@gmail.com>
2022-10-31 22:51:07 +0100

    complete page functionality

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const path = require('path');
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -13,11 +13,10 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
